309092bef4f7de77ccd8d4ecdcb5214aedb70714
idanhalp <idanhal27@gmail.com>
2025-10-20 21:06:48 +0300

    Refactored `utils::check_if_configurations_file_exists`.
    Call `std::filesystem::is_regular_file` instead of manually iterating over the root directory.

--- source/utils/utils.cpp
+++ source/utils/utils.cpp
@@ -10,25 +10,7 @@
 
 auto utils::check_if_configurations_file_exists(const std::filesystem::path& path) -> bool
 {
-    const auto path_is_valid = std::filesystem::exists(path) && std::filesystem::is_directory(path);
-
-    if (!path_is_valid)
-    {
-        return false;
-    }
-
-    for (const auto& entry : std::filesystem::directory_iterator(path))
-    {
-        const auto is_configurations_file =
-            entry.is_regular_file() && entry.path().filename() == params::CONFIGURATIONS_FILE_NAME;
-
-        if (is_configurations_file)
-        {
-            return true;
-        }
-    }
-
-    return false;
+    return std::filesystem::is_regular_file(path / params::CONFIGURATIONS_FILE_NAME);
 }
 
 auto utils::get_object_file_name(const std::filesystem::path& path) -> std::string
