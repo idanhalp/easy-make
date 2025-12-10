302dc279b72738d9d53cc0fee010b2515e8ac6d4
idanhalp <idanhal27@gmail.com>
2025-12-10 21:27:54 +0200

    Fixed mistakes in `update_version` script.

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -18,6 +18,11 @@ def get_arguments() -> tuple[int, int, int]:
     return int(major), int(minor), int(patch)
 
 
+current_branch = subprocess.run("git branch --show-current", shell=True, capture_output=True, text=True).stdout.strip()
+if current_branch != "development":
+    print("Must be on branch 'development' when pushing new version!")
+    sys.exit(1)
+
 major, minor, patch = get_arguments()
 version = f"{major}.{minor}.{patch}"
 tag_name = f"v{version}"
@@ -31,6 +36,10 @@ if tag_already_exists:
     sys.exit(1)
 
 subprocess.run(f'git commit --allow-empty -m "Updated version to {version}."', shell=True)
+subprocess.run(f"git commit --allow-empty --amend", shell=True)
+subprocess.run("git push origin development", shell=True)
 subprocess.run(f'git tag -a {tag_name} -m "Version {version}"', shell=True)
-subprocess.run("git push origin master")
-subprocess.run(f"git push origin {tag_name}")
+subprocess.run(f"git push origin {tag_name}", shell=True)
+subprocess.run(f"git switch master", shell=True)
+subprocess.run(f"git merge development", shell=True)
+subprocess.run("git push origin master", shell=True)
